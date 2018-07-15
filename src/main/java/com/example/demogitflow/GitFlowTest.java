fc0f11ddc6fadcb91ee8ff29fe44e96de522aa6b
henriSedjame <sedhjo@gmail.com>
2018-07-15 11:35:39 +0200

    ajout d'une classe test

---
+++ src/main/java/com/example/demogitflow/GitFlowTest.java
@@ -0,0 +1,10 @@
+package com.example.demogitflow;
+
+/**
+ * @Project demogitflow
+ * @Author Henri Joel SEDJAME
+ * @Date 15/07/2018
+ * @Class purposes : .......
+ */
+public class GitFlowTest {
+}
