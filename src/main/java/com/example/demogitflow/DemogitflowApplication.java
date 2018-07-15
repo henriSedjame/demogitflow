912432bb37b05d44b089f9d35d155bb9474a6310
henriSedjame <sedhjo@gmail.com>
2018-07-15 11:31:54 +0200

    Initial commit

---
+++ src/main/java/com/example/demogitflow/DemogitflowApplication.java
@@ -0,0 +1,12 @@
+package com.example.demogitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitflowApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemogitflowApplication.class, args);
+  }
+}
